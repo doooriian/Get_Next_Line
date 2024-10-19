6375fae9551561d741f3faabe95627bed1e609ee
Doooriian <dorian.ley91@gmail.com>
2024-10-19 17:41:06 +0200

    bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: doley <doley@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 15:07:40 by doley             #+#    #+#             */
+/*   Updated: 2024/10/19 17:24:59 by doley            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct      s_fd_list
+{
+    int             fd;
+    char            *stock;
+    struct s_fd_list *next;
+}                   t_fd_list;
+
+/* UTILS */
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+/* MAIN */
+char	*ft_read(int fd, char *buffer, char **stock);
+char	*ft_trim(char *line);
+char	*get_next_line(int fd);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+#endif
